f29da299c81ae1ae9e2ca67dccb7b13aa708e375
Guillaume LEDUC <gduc.37@gmail.com>
2023-02-28 14:48:13 +0100

    Branchement avec l'API

--- components/User/UserForm.tsx
+++ components/User/UserForm.tsx
@@ -1,26 +1,42 @@
 'use client'
 
 
-import { Employee } from '../../typing'
+import { useRouter } from 'next/navigation'
+import { User } from '../../typing'
 
 
-const deleteEmployee = (employeeId: number) => {
-  try {
-    fetch(`http://lyserofinance-env.eba-rpnjkdkg.us-east-1.elasticbeanstalk.com/removeemployee/${employeeId}`, {
-        method: 'DELETE'
+
+export const UserForm = ({user}: {user: User}) => {
+  const router = useRouter()
+  console.log(user)
+  const modifyUser = async (e: any) => {
+    e.preventdefault();
+    const formData = new FormData(e.currentTarget);
+    const entries = Object.fromEntries(formData.entries());
+    const body = {
+      "mail": entries.mail,
+      "motDePasse": user.motDePasse,
+      "nom": entries.nom,
+      "prenom": entries.prenom,
+    }
+    await fetch(`http://amie.labinno-mtech.fr/api/utilisateur/updateutilisateur/${user.idUtilisateur}`,{
+      method:'PUT',
+      body: JSON.stringify(body),
     });
-  } catch (err) {
-    throw new Error('Ça ne marche absolument pas !')
-  }
-}
 
-export const UserForm = ({employee}: {employee: Employee}) => {
+    router.refresh();
+  }
+
   return (
-            <form className='grid gap-2' id='employeeForm'>
-                <label className='capitalize'> {employee.firstName}</label>
-                <label className=' uppercase'> {employee.lastName}</label>
-                {/* <label> {employee.mail}</label> */}
-                <button className='btn btn-error text-base-100' type='submit' onClick={() => deleteEmployee(employee.id)}>Supprimer</button>
+            <form className='grid gap-2' onSubmit={modifyUser}>
+                <input type="text" defaultValue={user.idUtilisateur} className='hidden'/>
+                <input className='capitalize my-2 border-b outline-none' defaultValue={user.prenom} name='prenom'/>
+                <input className=' uppercase my-2 border-b outline-none' defaultValue={user.nom} name='nom' />
+                <input className='text-sm my-2 border-b outline-none'defaultValue={user.mail} name='mail' />
+                <div className="grid grid-cols-2 gap-2">
+                  <button className='btn btn-primary text-base-100' type='submit'>Modifier</button>
+                  <button className='btn btn-error' onClick={(e) => {e.preventDefault();fetch(`http://amie.labinno-mtech.fr/api/utilisateur/deleteutilisateur/${user.idUtilisateur}`,{method:'DELETE',})}}>Supprimer</button>
+                </div>
             </form>
   )
 }
